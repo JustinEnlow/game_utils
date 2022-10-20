14468b7882d74c6c2b2482374fca886aaf6ff2b1
JustinEnlow <user@pop-os.localdomain>
2022-10-19 21:30:01 -0700

    idk

--- src/toggle.rs
+++ src/toggle.rs
@@ -1,34 +1,43 @@
-pub struct Toggle{
+// is trait or struct better for this use case?
+
+pub trait Toggle{
+    //fn new(enabled: bool) -> Self;
+    fn enabled(self: &Self) -> bool;
+    fn toggle(self: &mut Self);//{self.enabled = !self.enabled()}
+}
+
+//pub struct Toggle{
+//    enabled: bool
+//}
+//impl Toggle{
+//    pub fn new(enabled: bool) -> Self{Self{enabled}}
+//    pub fn enabled(self: &Self) -> bool{self.enabled}
+//    pub fn toggle(self: &mut Self){self.enabled = !self.enabled}
+//}
+
+
+
+
+
+/*
+examples:
+
+pub struct Power{
     enabled: bool
 }
-impl Toggle{
+impl Toggle for Power{
     pub fn new(enabled: bool) -> Self{Self{enabled}}
     pub fn enabled(self: &Self) -> bool{self.enabled}
     pub fn toggle(self: &mut Self){self.enabled = !self.enabled}
 }
 
-// can this be replaced with a trait?
-/*
-    pub struct FlightControlSystem<'a, T>{
-        linear_assist: T,
-    }
-    impl<'a, T> FlightControlSystem<'a, T>
-        where T: Float + Toggle
-    {
-*/
-pub trait TToggle{
-    fn new(enabled: bool) -> Self;
-    fn enabled(self: &Self) -> bool;
-    fn toggle(self: &mut Self);//{self.enabled = !self.enabled()}
+pub struct GSafety{
+    enabled: bool,
+    max_acceleration: ControlAxis<Dimension3<AxisContribution<f32>>>
 }
-
-
-
-//pub struct Toggle{
-//    enabled: bool
-//}
-//impl TToggle for Toggle{
-//    fn new(enabled: bool) -> Self{Self{enabled}}
-//    fn enabled(self: &Self) -> bool{self.enabled}
-//    fn toggle(self: &mut Self){self.enabled = !self.enabled}
-//}
+impl Toggle for GSafety{
+    pub fn new(enabled: bool) -> Self{Self{enabled}}
+    pub fn enabled(self: &Self) -> bool{self.enabled}
+    pub fn toggle(self: &mut Self){self.enabled = !self.enabled}
+}
+*/
